aa5d797fb06427aa7d737fddbb57239a3c673fe1
TheAlexDev23 <thealexdev23@gmail.com>
2024-01-25 18:29:36 +0100

    fix: not tracking file after editing with neovim/vim feat: add config reload

--- synchronizer_service.py
+++ synchronizer_service.py
@@ -11,7 +11,7 @@ from datetime import datetime
 from inotify_simple import INotify, flags
 
 # Polling rate for file changes in seconds
-RATE = 1
+RATE = 0.5
 
 HOME = os.environ.get("HOME")
 if HOME is None:
@@ -22,49 +22,94 @@ CONFIG = HOME + "/.config/synchronization_targets.json"
 
 VERBOSE_LOGGING = False
 
+# Mainly used in development. If False, won't commit
+COMMIT = True
+
+config_notify: INotify
+dotfiles_notify: INotify
+
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
-full_paths = {}
+base_paths = {}
+all_tracked_paths = {}
+
+
+def track_config():
+    global config_notify
+    config_notify = INotify()
+    watch_flags = flags.CREATE | flags.DELETE | flags.MODIFY | flags.DELETE_SELF
+
+    config_notify.add_watch(CONFIG, watch_flags)
+
+
+def backup_initial_files():
+    print("Performing initial backup")
+
+    with open(CONFIG, "r") as fp:
+        data = json.load(fp)
+
+    for dir in data["directories"]:
+        dir = os.path.expanduser(dir)
+        backup_directory(dir)
+
+    for path in data["files"]:
+        path = os.path.expanduser(path)
+        backup_file(path)
+
+    git_commit("initial backup")
 
 
 def check_for_changes():
-    global dotfiles_notify
-
-    parse_config()
-
-    os.mkdir(HOME + "/.config/polybar/haha")
+    global dotfiles_notify, config_notify
 
     while True:
-        for change in dotfiles_notify.read():
-            if not VERBOSE_LOGGING:
-                continue
-            print(change + " " + get_full_path(change.wd))
-            for flag in flags.from_mask(change.mask):
-                print(next((flag.name for _flag in flags if _flag.value == flag), None))
-
-        # git_commit()
+        check_config_changes()
+        check_tracked_files()
 
         time.sleep(RATE)
 
 
-def parse_config():
+def parse_config(exit_on_failure=True):
     try:
         with open(CONFIG, "r") as fp:
             data = json.load(fp)
-
-        global dotfiles_notify
     except:
-        eprint("Could not load config. Exiting...")
-        exit(1)
+        eprint("Could not load config. ")
+        if exit_on_failure:
+            exit(1)
+        else:
+            # Gotta exit and keep previous dotfiles_notify because we wont be able to parse data
+            return
 
-    dotfiles_notify = get_inotify(data)
+    track_dotfiles(data)
 
 
-def get_inotify(data) -> INotify:
-    inotify = INotify()
+def track_dotfiles(data) -> None:
+    global dotfiles_notify
+    dotfiles_notify = INotify()
+
+    for path in data["directories"]:
+        path = os.path.expanduser(path)
+        base_path = path
+        print(f"Watching {path}")
+        add_watch(path, base_path)
+
+        for root, dirs, _ in os.walk(path):
+            for directory in dirs:
+                path = os.path.join(root, directory)
+                print(f"Watching {path}")
+                add_watch(path, base_path)
+
+    for path in data["files"]:
+        path = os.path.expanduser(path)
+        print(f"Watching {path}")
+        add_watch(path, path)
+
+
+def add_watch(path: str, base_path: str) -> None:
     watch_flags = (
         flags.CREATE
         | flags.DELETE
@@ -74,69 +119,129 @@ def get_inotify(data) -> INotify:
         | flags.MOVED_TO
     )
 
-    for path in data["directories"]:
-        path = os.path.expanduser(path)
-        print(f"Watching {path}")
-        wd = inotify.add_watch(path, watch_flags)
-        add_to_full_paths(path, wd)
-
-        for root, dirs, _ in os.walk(path):
-            for directory in dirs:
-                path = os.path.join(root, directory)
-                print(f"Watching {path}")
-                wd = inotify.add_watch(path, watch_flags)
-                add_to_full_paths(path, wd)
-
-    for path in data["files"]:
-        path = os.path.expanduser(path)
-        print(f"Watching {path}")
-        inotify.add_watch(path, watch_flags)
-
-    return inotify
+    global dotfiles_notify
+    wd = dotfiles_notify.add_watch(path, watch_flags)
+    add_to_base_paths(base_path, wd)
+    all_tracked_paths[wd] = path
 
 
-def add_to_full_paths(path: str, wd):
-    if path not in full_paths:
-        full_paths[path] = []
+def add_to_base_paths(path: str, wd):
+    if path not in base_paths:
+        base_paths[path] = []
 
-    full_paths[path].append(wd)
+    base_paths[path].append(wd)
 
 
-def get_full_path(wd) -> str | None:
-    for key, value in full_paths.items():
+def check_config_changes():
+    global config_notify
+
+    changes = config_notify.read(0, 1)
+
+    if len(changes) == 0:
+        return
+
+    for change in changes:
+        for flag in flags.from_mask(change.mask):
+            if flag is flags.IGNORED:
+                track_config()
+
+    parse_config(False)
+
+
+def check_tracked_files():
+    global dotfiles_notify
+
+    changes = dotfiles_notify.read(0, 1)
+
+    if len(changes) == 0:
+        return
+
+    changed_dirs = set()
+    for change in changes:
+        base_path = get_base_path(change.wd)
+        print(f"Detected changes in {base_path}")
+        changed_dirs.add(base_path)
+
+        if VERBOSE_LOGGING:
+            print(f"Change {change}")
+            log_change_flags(change)
+
+        retrack_file_if_necessary(change, base_path)
+
+    for change in changed_dirs:
+        if os.path.isdir(change):
+            backup_directory(change)
+        else:
+            backup_file(change)
+
+    commit_message = "Changes to "
+    for change in changed_dirs:
+        commit_message += os.path.basename(change) + " "
+
+    git_commit(commit_message)
+
+
+def get_base_path(wd) -> str:
+    for key, value in base_paths.items():
         if wd in value:
             return key
-    return None
+
+    raise ValueError("Invalid argument")
+
+
+def log_change_flags(change):
+    for flag in flags.from_mask(change.mask):
+        print(next((flag.name for _flag in flags if _flag.value == flag), None))
+
+
+def retrack_file_if_necessary(change, base_path):
+    for flag in flags.from_mask(change.mask):
+        if flag is not flags.IGNORED:
+            continue
+
+        path = all_tracked_paths[change.wd]
+
+        add_watch(path, base_path)
 
 
 def backup_directory(dir: str) -> None:
-    print(f"Change in directory {dir}")
+    print(f"Backing up directory {dir}")
 
     backup_dir = dir.replace(HOME, "./home")
 
     subprocess.run(["mkdir", "-p", backup_dir])
     subprocess.run(["cp", "-r", dir, backup_dir + "/.."])
 
-    git_commit()
-
 
 def backup_file(path: str) -> None:
-    print(f"Change in file {path}")
+    print(f"Backing up file {path}")
 
     backup_dir = os.path.dirname(path.replace(HOME, "./home"))
 
     subprocess.run(["mkdir", "-p", backup_dir])
     subprocess.run(["cp", path, backup_dir])
 
-    git_commit()
 
+def git_commit(commit_message: str | None = None):
+    if not COMMIT:
+        return
 
-def git_commit():
-    current_datetime = datetime.now()
+    if commit_message is None:
+        commit_message = str(datetime.now())
 
     subprocess.run(["git", "add", "."])
-    subprocess.run(["git", "commit", "-m", f"Automated update {current_datetime}"])
-    subprocess.run(["git", "push", "origin", "main"])
+    subprocess.run(["git", "commit", "-m", f"Automated update: {commit_message}"])
+
+    git_push()
 
 
-check_for_changes()
+def git_push():
+    subprocess.run(["git", "push"])
+
+
+if __name__ == "__main__":
+    parse_config()
+    track_config()
+
+    backup_initial_files()
+    check_for_changes()
